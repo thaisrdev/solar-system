44f6514c94f9eddd6526f96988d11873d868b9c6
Thais Rosa <tsrosa18@gmail.com>
2022-08-18 18:00:59 -0300

    requisito 2

---
+++ src/components/SolarSystem.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+// import './Header.css';
+
+class SolarSystem extends React.Component {
+  render() {
+    return (
+      <div data-testid="solar-system" />
+    );
+  }
+}
+
+export default SolarSystem;
