cc48fc47b31a577c438d872b21777325daea48ce
Thais Rosa <tsrosa18@gmail.com>
2022-08-18 17:56:33 -0300

    corrigindo erro

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import Header from './components/Header';
 import './App.css';
+import SolarSystem from './components/SolarSystem';
 
 class App extends React.Component {
   render() {
     return (
       <div>
         <Header />
+        <SolarSystem />
       </div>
     );
   }
